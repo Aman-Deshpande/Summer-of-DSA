80c4692a329045b2efa57fcc0f75afe306c0e6af
Aman Deshpande <amandeshpande392@gmail.com>
2022-06-03 09:51:19 +0530

    Day 1 -> 2 June

---
+++ Arrays/Question06.cpp
@@ -0,0 +1,26 @@
+// Best Time to Buy and Sell Stock
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int leastSofar = INT_MAX; // least price so far
+        int totalProfit = 0; // total profit
+        int profitOngivenDay = 0; // profit if sold on a given day
+        
+        for(int i = 0; i<prices.size(); i++){
+            if(prices[i] < leastSofar){
+                leastSofar = prices[i];
+            }
+            
+            profitOngivenDay = prices[i]-leastSofar;
+            if(totalProfit < profitOngivenDay){
+                totalProfit = profitOngivenDay;
+            }
+        }
+        
+        return totalProfit;
+    }
+}; 
